a7a0a7cb650ea624a716c29386a080b17bb56eb9
SuryaTeja-koka <sherlockhomes391@gmail.com>
2021-06-22 12:18:44 +0530

    commit
    commit

--- mytrisul/settings.py
+++ mytrisul/settings.py
@@ -10,13 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from django.db.backends.mysql.base import DatabaseWrapper
+# from django.db.backends.mysql.base import DatabaseWrapper
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DatabaseWrapper.data_types['DateTimeField'] = 'datetime'
+# DatabaseWrapper.data_types['DateTimeField'] = 'datetime'
 
 
 # Quick-start development settings - unsuitable for production
